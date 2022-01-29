3a06611961c71d326dc9bf0a1a6572386e508c51
Emre <Emre@DESKTOP-THOD234>
2022-01-29 15:09:01 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext//miras-inheritance
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+        {
+            OptionsBuilder.UseSqlServer("server=DESKTOP-THOD234;database=CoreBlogDb; integrated security=true;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnailImage { get; set; }
+        public string BlogImage { get; set; }
+        public DateTime BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+
+
+        //category
+        public int CategoryID { get; set; }
+        public  Category Category{ get; set; }
+
+        //comment
+        public List<Comment> Comments { get; set; }//bir bloga birden fazla yorum yapılabilir
+
+    }
+}
